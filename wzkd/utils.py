86905ec2ec09f5fa4d1d8d9061b85a3ef4c10436
gentil_renard <gentil_renard@pop-os.localdomain>
2022-04-21 20:22:26 +0200

    deletion of old api format modules

--- wzkd/utils.py
+++ wzkd/utils.py
@@ -1,13 +1,177 @@
 import datetime
 from datetime import datetime, timezone
+from doctest import DocFileTest
 import pandas as pd
 import numpy as np
+import toml
+import json
 
-import streamlit as st
 
-  
-  
-# Getting the current date
+def load_labels(file="wz_labels.json"):
+    """--> dict, with data needed to parse weapons, games modes etc."""
+    with open(file) as f:
+        LABELS = json.load(f)
+    return LABELS
+
+
+def load_conf(file="conf.toml"):
+    """--> dict, overall app config : run in offline mode, display Battle Royale games only etc."""
+    with open(file) as f:
+        CONF = toml.load(f)
+    return CONF
+
+
+def extract_loadouts(df, CONF):
+    """
+    Extract player(s) loadout(s) from match(es) df > player col > 'loadout' (or 'loadouts')
+    Flatten (breakdown) them into one or several cols : one for every loadout
+    n of loadouts to extract defined in conf.toml
+
+    Parameters
+    ----------
+    df, match result previously converted as a df, with 'player' col already expanded
+
+    Returns
+    -------
+    Dataframe, with n columns of players loadout(s), that we will append to our main df
+    """
+
+    loadout_col = "loadout" if "loadout" in df.columns.tolist() else "loadouts"
+    # when extracting, df 'player' col is already expanded, with 'loadout' accessible
+    df_loadouts = df[loadout_col].apply(pd.Series)
+
+    # max number of payloads to parse, as defined in conf
+    n_loadouts = CONF.get("PARSING")["n_loadouts"]
+    n_loadouts = (
+        n_loadouts
+        if n_loadouts <= len(df_loadouts.columns)
+        else len(df_loadouts.columns)
+    )
+
+    # remove the extra loadouts ( > max loadouts to keep)
+    df_loadouts = df_loadouts.iloc[:, 0:n_loadouts]
+
+    # rename final columns : loadout_1, loadout_2 ...
+    col_names = {idx: f"loadout_{idx+1}" for idx, col in enumerate(df_loadouts.columns)}
+    df_loadouts.rename(columns=col_names, inplace=True)
+
+    return df_loadouts
+
+
+def parse_loadout(loadout_value, LABELS):
+    """
+    Parse a loadout entry (dict),  extract weapons names then rename using wzlabels.json
+
+    Parameters
+    ----------
+    loadout : a dict value after we flattened a match(es) result / extracted loadouts entries.
+        e.g.
+        dict{
+                primaryWeapon:{name:s4_pi_mike1911...},
+                perks:[...],
+                other keys,,
+            }
+
+    Returns
+    -------
+    String, parsed primary and secondary weapons names
+    """
+
+    def extract_weapons(loadout_value):
+        return f"{loadout_value.get('primaryWeapon')['name']} {loadout_value.get('secondaryWeapon')['name']}"
+
+    def parse_weapons(weapons, LABELS):
+        list_weapons = weapons.split(" ")
+        for PREFIX in LABELS["weapons"].get("prefixes"):
+            list_weapons = list(
+                map(lambda weapon: weapon.replace(PREFIX, ""), list_weapons)
+            )
+        list_weapons = [
+            weapon.replace(weapon, LABELS["weapons"]["names"].get(weapon, weapon))
+            for weapon in list_weapons
+        ]
+        return " ".join(list_weapons)
+
+    if pd.isnull(loadout_value):
+        return np.nan
+    else:
+        weapons = extract_weapons(loadout_value)
+        return parse_weapons(weapons, LABELS)
+
+
+def extract_missions(df):
+    """
+    Flatten then extract (desired) mission stats from matche(s) df > player col > 'brMissionStats'
+
+    Parameters
+    ----------
+    df, match(es) result previously converted as a df, with 'player'/' col already expanded
+
+    Returns
+    -------
+    Dataframe, with desired missions KPIs as columns, that we will append to our main df
+    """
+
+    main_col = "brMissionStats"
+    sub_col = "missionStatsByType"
+    df_missions = df[main_col].apply(pd.Series)
+
+    return pd.concat(
+        [df_missions.drop([sub_col], axis=1), df_missions[sub_col].apply(pd.Series)],
+        axis=1,
+    )
+
+
+def parse_mission(mission_value, CONF):
+    """
+    Parse a mission entry (dict), extracting 'count'
+
+    Parameters
+    ----------
+    mission_value : a dict value after we flattened a match(es) result/'player'/'brMissionStats'
+        e.g.
+        dict{
+                weaponXp:650.0},
+                xp:650,
+                count:1
+            }
+
+    Returns
+    -------
+    int, count of a given mission
+    """
+
+    if pd.isnull(mission_value):
+        return np.nan
+    else:
+        return mission_value["count"]
+
+
+def parse_gulag(gulagKills):
+    """
+    Parse a gulagKills entry with a value either NaN, 1 or 0, to W, L, NaN
+    """
+    if pd.isnull(gulagKills):
+        return np.nan
+    else:
+        return "W" if gulagKills == 1 else "L"
+
+
+def get_last_match_Id(matches):
+    """Extract last (Battle Royale) Match ID from list of last matches"""
+    list_br_match = [match for match in matches if "br_br" in match["mode"]]
+    last_match_id = int(list_br_match[0]["matchID"]) if len(list_br_match) > 0 else None
+    return last_match_id
+
+
+def get_gamer_tag(matches):
+    """
+    A player can be searchable with a given name but having a different gamertag in-game
+    We need that gamertag if we were to analyse the "one match stats" endpoint (as done in match_format.py)
+    from player's perspective
+    """
+    return matches[0]["player"]["username"]
+
 
 def DatetimeToTimestamp(datetime):
     """
@@ -16,7 +180,7 @@ def DatetimeToTimestamp(datetime):
     Parameters
     ----------
     date : datetime
-        datetime(yyyy, mm, dd) 
+        datetime(yyyy, mm, dd)
 
     Returns
     -------
@@ -24,10 +188,11 @@ def DatetimeToTimestamp(datetime):
         UTC timestamp (milliseconds)
 
     """
-    
+
     date = datetime.now(timezone.utc)
-    
-    return  int(date.timestamp() * 1000)
+
+    return int(date.timestamp() * 1000)
+
 
 def TimestampToDatetime(timestamp):
     """
@@ -36,7 +201,7 @@ def TimestampToDatetime(timestamp):
     Parameters
     ----------
     date : int
-        timestamp 
+        timestamp
 
     Returns
     -------
@@ -44,210 +209,5 @@ def TimestampToDatetime(timestamp):
         datetime Object
 
     """
-    
-    return  datetime.fromtimestamp(timestamp)
 
-#st.cache
-def MatchesToDf(matches):
-    
-    keep_cols =  [
-        'mode',
-        'utcStartSeconds',
-        'utcEndSeconds',
-        'timePlayed',
-        'teamPlacement',
-        'kdRatio', 
-        'kills', 
-        'deaths', 
-        'assists', 
-        'damageDone',
-        'damageTaken',
-        'gulagKills',
-        'percentTimeMoving',
-        'duration'
-        ]
-
-    df = pd.DataFrame(matches)
-    
-    # column playerStats is a series of dict, we can expand it easily and append, then drop the original
-    df = pd.concat([df.drop(['playerStats'], axis=1), df['playerStats'].apply(pd.Series)], axis=1)
-    
-    # colum player has more depth
-    # once expanded, it has a column 'loadout' : a series of list of dict (either one or more, we will keep 1 to max 3)
-    # and also brMissionStats (mostly empty ?) that we aren't interested in
-    
-    df = pd.concat([df.drop(['player'], axis=1), df['player'].apply(pd.Series)], axis=1)
-    df = df.drop(['brMissionStats'], axis = 1)
-    df = pd.concat([df.drop(['loadout'], axis=1), df['loadout'].apply(pd.Series)], axis=1)
-    for col in range(0,3):
-        if col in df.columns:
-            df[col] = df[col].apply(lambda x: f"{x['primaryWeapon']['name']} - {x['secondaryWeapon']['name']}" if not str(x) == 'nan' else np.nan)
-            col_name = "loadout_" + str(col +1)
-            df = df.rename(columns={col: f"loadout_{str(col +1)}"})   
-            keep_cols.append(col_name)
-    
-    return df[keep_cols]
-
-#st.cache
-def MatchesStandardize(df):
-    
-    int_cols =  [
-        'teamPlacement', 
-        'kills', 
-        'deaths', 
-        'assists', 
-        'gulagKills', 
-        'damageDone',
-        'damageTaken'
-        ]
-    
-    float_cols = [
-        'kdRatio',
-        'percentTimeMoving'
-        ]
-    
-    ts_cols = [
-        'utcStartSeconds',
-        'utcEndSeconds'
-        ]
-    
-    mode_labels = {
-        'br_brtrios':'Trios',
-        'br_brduos':'Duos',
-        'br_brquads':'Quads',
-        'br_dbd_dbd':'Iron Trials',
-        'br_dmz_plunquad':'Pldr x4',
-        'br_rumble_clash':'Rumble'
-
-        }
-    
-    columns_labels = {
-        'utcEndSeconds':'Ended at',
-        'utcStartSeconds':'Started at',
-        'timePlayed': 'Playtime',
-        'teamPlacement':'#',
-        'kdRatio':'KD',
-        'damageDone':'Damage >',
-        'damageTaken':'Damage <',
-        'gulagKills':'Gulag',
-        'headshots':'% headshots',
-        'percentTimeMoving':'% moving',
-        'duration':'Game duration'
-        }
-
-    # df = df.fillna(0)
-    df[int_cols] = df[int_cols].astype(int)
-    df[float_cols] = df[float_cols].astype(float).round(1) # still renders 0.0000 in streamlit but ugly hacks exists
-    
-    # specials
-    df['utcEndSeconds'] = df['utcEndSeconds'].apply(lambda x: datetime.fromtimestamp(x))
-    df['utcStartSeconds'] = df['utcStartSeconds'].apply(lambda x: datetime.fromtimestamp(x))
-    
-    df['duration'] = df['duration'].apply(lambda x: x/1000).apply(lambda x: pd.to_datetime(x, unit='s').strftime('%M')) # API duration is in seconds x1000
-    df['timePlayed'] = df['timePlayed'].apply(lambda x: pd.to_datetime(x, unit='s').strftime('%M:%S')) # API timePlayed is in seconds
-    df['gulagKills'] = df['gulagKills'].map({1:'W', 0:'L'})
-    for col in ['loadout_1', 'loadout_2', 'loadout_3']:
-            df.fillna({col:'-'}, inplace=True) if col in df.columns else None
-    
-    df = df.replace({"mode": mode_labels})
-    df = df.rename(columns=columns_labels)
-    df.columns = df.columns.str.capitalize()
-    df = df.rename({"Kd":"KD"}, axis=1)
-     
-#st.cache
-def MatchesPerDay(df):
-    """
-    Streamlit/basic AgGrid does not render well (aka blank rows etc.) multi indexed df
-    So we organize data differently, though more complex ^_^
-    
-    Returns
-    -------
-    {
-        "str_weekday_1":df-of-matches-that-day,
-        "str_weekday_2":df-of-matches-that-day,
-        (...)
-    }
-
-    """
-    
-    drop_cols =  [
-        'Started at',
-        'Playtime',
-        '% moving',
-        'Game duration'
-        ]
-    
-    keep_cols = [
-        'End time',
-        'Mode',
-        '#',
-        'KD',
-        'Kills',
-        'Deaths',
-        'Assists',
-        'Damage >',
-        'Damage <',
-        'Gulag'
-    ]
-    
-    loadout_cols = df.columns[df.columns.str.startswith('Loadout')].tolist()
-    
-    # 1. --- initial formating : datetime, concat loadouts cols ---
-    
-    df = df.drop(drop_cols, axis = 1)
-    df['End time'] = df['Ended at'].dt.time
-    
-    def concat_loadouts(df, columns):
-        return pd.Series(map('_'.join, df[columns].values.tolist()),index = df.index)
-    
-    df['Weapons'] = concat_loadouts(df, loadout_cols)
-    keep_cols = [*keep_cols, *['Weapons']]
-    
-    # 2. --- build result {"day1": df-matches-that-day, "day2": df...} ---
-    
-    list_df = [g for n, g in df.groupby(pd.Grouper(key='Ended at',freq='D'))]
-    list_df = [df for df in list_df if not df.empty]
-    list_days = [df['Ended at'].tolist()[0].strftime('%A') for df in list_df]
-    
-    # make sure we display latest day first, then build dictionary
-    for list_ in [list_days, list_df]:
-        list_.reverse()
-    day_matches = dict(zip(list_days, list_df))
-    
-    # 3. --- some more (re)formating ---
-    
-    # for some reason (me ? ^-^, Grouper => Series?) couldnt' modify df before building the result, must iterate again 
-    for k, v in day_matches.items():
-        day_matches[k] = day_matches[k][keep_cols]
-      
-    return day_matches
-
-
-def AggStats(df):
-    """
-    We want to add aggregated stats for each day/df of matches we got from MatchesPerDay()
-    
-    Returns
-    -------
-    {
-    'Kills':total n kills that day,
-    'Deaths': total deaths,
-    'KD': kills/deaths,
-    'Gulags': win %,
-    'Played': count matches
-    }
-    
-    """
-
-    agg_func = {
-        "Mode":"count",
-        "Kills":"sum",
-        "Deaths":"sum"
-    } 
-    dict_ = df.agg(agg_func).to_dict()
-    dict_.update({'KD': (df.Kills.sum() / df.Deaths.sum()).round(2)})
-    dict_.update({'Gulags': int((df.Gulag.str.count("W").sum() / df.Gulag.str.count("L").sum())*100)})
-    dict_['Played'] = dict_.pop('Mode')
-    
-    
-    return dict_
+    return datetime.fromtimestamp(timestamp)
